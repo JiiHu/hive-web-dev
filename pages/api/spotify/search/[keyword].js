144ae2c0f0211c3a81054e0f264fa0229445f84f
Jonne <jonne.huotari@perfektio.fi>
2023-02-22 15:09:17 +0200

    Add try/catch

--- pages/api/spotify/search/[keyword].js
+++ pages/api/spotify/search/[keyword].js
@@ -2,20 +2,24 @@ import SpotifyWebApi from 'spotify-web-api-node'
 import { getSession } from 'next-auth/react'
 
 const handler = async (req, res) => {
-  const {
-    query: { keyword }
-  } = req
-  const session = await getSession({ req })
+  try {
+    const {
+      query: { keyword }
+    } = req
+    const session = await getSession({ req })
 
-  const spotifyApi = new SpotifyWebApi()
-  spotifyApi.setAccessToken(session.accessToken)
+    const spotifyApi = new SpotifyWebApi()
+    spotifyApi.setAccessToken(session.accessToken)
 
-  const result = await spotifyApi.searchTracks(keyword)
+    const result = await spotifyApi.searchTracks(keyword)
 
-  if (result.statusCode === 200) {
-    res.status(200).json({ success: true, data: result.body.tracks })
-  } else {
-    res.status(500).json({ success: false })
+    if (result.statusCode === 200) {
+      res.status(200).json({ success: true, data: result.body.tracks })
+    } else {
+      res.status(500).json({ success: false })
+    }
+  } catch (e) {
+    res.status(500).json({ success: false, message: e.message })
   }
 }
 
